d146d92657955fa6c1f30ad71ae46f5a6d6fe3e4
yin <yintothayang@gmail.com>
2019-04-23 23:18:42 -0700

    add screen, looks like ScreenManager has changed

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,11 +1,18 @@
-import Ludic, { LudicOptions } from '@ludic/ludic'
+import Ludic, { LudicOptions, ScreenManager } from '@ludic/ludic'
+import GameScreen from './screens/GameScreen'
 
 export default class Arce extends Ludic {
+  public screenManager: ScreenManager
 
   constructor(opts: LudicOptions){
     super(opts)
+
+    this.screenManager = new ScreenManager(this)
+    this.screenManager.addScreen(new GameScreen())
   }
 
-  update(){
+  update(delta: number){
+    console.log("updating")
+    this.screenManager.update(delta)
   }
 }
---
+++ src/game/screens/GameScreen.ts
@@ -0,0 +1,16 @@
+import {Screen} from '@ludic/ludic'
+
+
+export default class GameScreen extends Screen {
+  constructor() {
+    super()
+  }
+
+  public onAddedToManager() {
+
+  }
+
+  public update(delta: number){
+
+  }
+}
