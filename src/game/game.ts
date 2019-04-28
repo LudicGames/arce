c651c0c550577b4604b07cb94cdc03a22c78af7f
yin <yintothayang@gmail.com>
2019-04-28 12:15:55 -0700

    game stuff

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,22 +1,34 @@
-import Ludic, { LudicOptions, ScreenManager } from '@ludic/ludic'
+import Ludic, { LudicOptions, ScreenManager, ScreenManagerListener, Screen } from '@ludic/ludic'
 import GameScreen from './screens/GameScreen'
 
-export default class Arce extends Ludic {
+export default class Arce extends Ludic implements ScreenManagerListener {
   public screenManager: ScreenManager
 
   constructor(opts: LudicOptions){
+    console.log("constructor", opts)
     super(opts)
 
-    this.screenManager = new ScreenManager(this)
-
-    // TODO
-    // this.screenManager.addScreenEventListener(this)
+    this.screenManager = new ScreenManager()
+    this.screenManager.addScreenEventListener(this)
     this.screenManager.addScreen(new GameScreen())
   }
 
+  onScreenFinished(screen: Screen, manager: ScreenManager, finalData: Screen['_finalData']): void {
+
+  }
+  onScreenAdded(screen: Screen, manager: ScreenManager, replace: boolean): void {
+    console.log("onScreenAdded: ", screen)
+  }
+  onScreensRemoved(screens: Screen[], manager: ScreenManager): void {
+
+  }
+
+  onWarnPopScreen(stack: Screen[], manager: ScreenManager): void {
+
+  }
 
   update(delta: number){
-    console.log("updating")
+    // console.log("updating")
     this.screenManager.update(delta)
   }
 }
