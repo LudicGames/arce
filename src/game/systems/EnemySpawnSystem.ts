cac7025d0aa795be2bd18c55564871078836f0af
yin <yintothayang@gmail.com>
2019-10-23 23:51:16 -0700

    survival mode

--- src/game/systems/EnemySpawnSystem.ts
+++ src/game/systems/EnemySpawnSystem.ts
@@ -9,6 +9,7 @@ import { PositionComponent,
          SizeComponent,
          SpeedComponent,
          DestinationCubeComponent,
+         CameraComponent,
          PreviousCubeComponent,
          CubeCoordinateComponent,
          EnemyConfigComponent
@@ -23,24 +24,34 @@ export default class EnemySpawnSystem extends System {
   queries: {
     enemyConfig: QueryType
     tiles:  QueryType
-
+    camera: QueryType
   }
 
-  spawns = [1, 2, 3, 4, 5]
+  spawning = false
 
   execute(deltaTime: number, time: number): void {
     const enemyConfig: Map = this.queries.enemyConfig.results[0].getComponent(EnemyConfigComponent).value
     const tileSize: number = this.queries.tiles.results[0].getComponent(SizeComponent).value
+    const camera: Camera = this.queries.camera.results[0].getComponent(CameraComponent).value
+    const ptm: number = camera.pixelsToMeters
+    const h: number = Math.ceil(camera.height / ptm)
+    const w: number = Math.ceil(camera.width / ptm)
 
-    // Just spawn a random enemy every 10 seconds
-    if(this.spawns.indexOf(Math.round(time / 5000)) > -1){
-      this.spawnRandomEnemy(tileSize)
-      this.spawns.splice(0, 1)
+    // Just spawn a random enemy every 3 second
+    if(!this.spawning){
+      this.spawning = true
+      setTimeout(()=>{
+        const x = this.rand(-w/2, w/2)
+        const y = this.rand(-h/2, h/2)
+
+        this.spawnRandomEnemy(tileSize, x, y)
+        this.spawning = false
+      }, 3000)
     }
   }
 
-  spawnRandomEnemy(tileSize: number){
-    let pos: Vector2 = new Vector2(-tileSize * 8, tileSize * 5)
+  spawnRandomEnemy(tileSize: number, x: number, y: number){
+    let pos: Vector2 = new Vector2(x, y)
     let cube: CubeCoordinate = vector2_to_cube(pos, tileSize)
     this.world.createEntity()
       .addComponent(PositionComponent, pos)
@@ -52,6 +63,10 @@ export default class EnemySpawnSystem extends System {
       .addComponent(PreviousCubeComponent, cube)
       .addComponent(isEnemyComponent)
   }
+
+  rand(min: number, max: number) {
+    return Math.random() * (max - min) + min
+  }
 }
 
 
@@ -59,4 +74,5 @@ export default class EnemySpawnSystem extends System {
 EnemySpawnSystem.queries = {
   enemyConfig: { components: [EnemyConfigComponent], mandatory: true},
   tiles:  { components: [isTileComponent], mandatory: true},
+  camera: { components: [CameraComponent], mandatory: true},
 }
